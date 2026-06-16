58cddefe9f291a0e067b9579a9936c87e432fcbd
Abeni <abenezeramare238@gmail.com>
2026-06-16 20:04:17 +0300

    4th

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
